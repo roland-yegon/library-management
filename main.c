f356b3cd8868ded340ea170577041485ad122138
Roland Yegon <rolandyegon@gmail.com>
2026-03-17 18:56:06 +0300

    Defined some variables

--- main.c
+++ main.c
@@ -11,3 +11,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_BOOKS    100
+#define MAX_BORROWS  200
+#define TITLE_LEN     80
+#define AUTHOR_LEN    60
+#define ID_LEN        10
+#define NAME_LEN      60
