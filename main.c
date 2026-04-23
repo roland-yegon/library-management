481f5493728c253c2a09c43562e6cdb42ced8265
Roland Yegon <rolandyegon@gmail.com>
2026-04-23 10:56:38 +0300

    removed unneccessary comments

--- main.c
+++ main.c
@@ -1,12 +1,3 @@
-/*
- * ============================================================
- *  LIBRARY MANAGEMENT SYSTEM
- *  Technical University of Mombasa
- *  CSE 4101 - Introduction to Programming
- *  Module: Library (4.6)
- * ============================================================
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
